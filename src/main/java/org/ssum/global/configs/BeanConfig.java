437577245c7fda246c159ec3be158b71232ee484
sswum <l5450won@naver.com>
2024-08-16 11:38:54 +0900

    JPA쿼리 팩토리 생성

--- src/main/java/org/ssum/global/configs/BeanConfig.java
+++ src/main/java/org/ssum/global/configs/BeanConfig.java
@@ -2,12 +2,24 @@ package org.ssum.global.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 public class BeanConfig {
+
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+        //JPA쿼리 팩토리 생성 : 쿼리를 계속 생성할려고 하는 거 아닌징?
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
